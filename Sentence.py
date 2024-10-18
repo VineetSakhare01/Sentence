13f318807af5facf899fdc76b1c480270d3fd80f
Vineet Sakhare <Vineetsakhare01@gmail.com>
2024-10-18 19:39:27 +0530

    Add files via upload

---
+++ Sentence.py
@@ -0,0 +1,8 @@
+
+# Q2. For a business create 3 variables to store-name, age, and city. then print a sentence that uses these variables.
+
+name = "Dhanesh"
+age = 18
+city = "Nashik"
+
+print("I'm", name, "my age is",age, "I am from", city)
